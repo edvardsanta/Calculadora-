4c4c52c4cf83117558d81042367e05eddbf06758
eduardo <edu@localhost.localdomain>
2022-03-16 19:23:59 -0300

    Starting project: Vite, Bootstrap and React

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Container, Row} from 'react-bootstrap'
+import './App.css'
+class App extends Component
+{
+  render()
+  {
+    return(
+	<Container>
+	<Row>
+ 	<div classname="calculator">
+	
+	</div>
+	</Row>
+	</Container>
+    )
+  }
+}
+
+export default App
